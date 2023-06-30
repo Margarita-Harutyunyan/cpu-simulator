2607add4a17764e63418f2b736a455644a9e8b72
Margarita-Harutyunyan <107138430+Margarita-Harutyunyan@users.noreply.github.com>
2023-06-30 15:57:04 +0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        FakeCPU fakeCPU = new FakeCPU();
-        fakeCPU.run("/home/molly/sr-90/other-cpu-simpulator/assembly.txt");
+        CPU cpu = new CPU();
+        cpu.run("/home/molly/sr-90/other-cpu-simpulator/assembly.txt");
     }
 }
